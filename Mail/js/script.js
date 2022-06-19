8913ee34c6601f9bc2b1745683be68e779d89510
markcaf <marcocafassog@gmail.com>
2022-06-20 01:09:50 +0200

    Initialization of user's email through a prompt

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,4 +1,7 @@
 // Inizializzazione array contenente le email approvate
-const mailApproved = [ "topolino@gmail.com", "paperino@gmail.com", "pippo@gmail.com", "pluto@gmail.com"];
+const mailApproved = ["topolino@gmail.com", "paperino@gmail.com", "pippo@gmail.com", "pluto@gmail.com"];
 console.log(mailApproved);
 
+// Inizializzazione variabile della mail digitata dall'utente tramite prompt
+const userMail = prompt("Inserisci la tua mail");
+console.log("Indirizzo email inserito: " + userMail);
