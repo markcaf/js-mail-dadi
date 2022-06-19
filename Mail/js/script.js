f15464d9b21fa30f3870ed5f68db23991021567b
markcaf <marcocafassog@gmail.com>
2022-06-20 01:05:37 +0200

    Array initialization containing approved emails

---
+++ Mail/js/script.js
@@ -0,0 +1,4 @@
+// Inizializzazione array contenente le email approvate
+const mailApproved = [ "topolino@gmail.com", "paperino@gmail.com", "pippo@gmail.com", "pluto@gmail.com"];
+console.log(mailApproved);
+
