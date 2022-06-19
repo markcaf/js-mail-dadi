17ee6d2a824f67a9c8d2030ab9c9b5142b09576e
markcaf <marcocafassog@gmail.com>
2022-06-20 00:29:20 +0200

    game's explanation and declaration of variables

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,6 @@
+// Spiegazione del Gioco dei Dadi con output in console
+console.log("Il gioco consiste nel lancio di un dado, sia per il giocatore sia per il computer. Si aggiudicherà la vittoria chi fa il punteggio più alto.")
+
+// Dichiarazione delle variabili per User e CPU
+let userNumber;
+let cpuNumber;
