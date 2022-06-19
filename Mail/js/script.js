bdc0ca98de24b1a830f39636374934c5cd1629b1
markcaf <marcocafassog@gmail.com>
2022-06-20 01:16:35 +0200

    For loop, if the user's mail matches the approved

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -5,3 +5,19 @@ console.log(mailApproved);
 // Inizializzazione variabile della mail digitata dall'utente tramite prompt
 const userMail = prompt("Inserisci la tua mail");
 console.log("Indirizzo email inserito: " + userMail);
+
+// Inizializzazione della variabile di verifica email e messaggio di output
+let mailVerified = false;
+let message;
+
+// Ciclo for per verificare se la mail inserita corrisponde a quelle approvate
+for (let i = 0; i < mailApproved.length ; i++){
+
+    // Dichiarazione della variabile "verified" per rendere possibile la verifica con l'email utente
+    const verified = mailApproved[i];
+
+    // Condizione di verifica per identità
+    if ( userMail === verified ){
+        mailVerified = true;
+    }
+}
